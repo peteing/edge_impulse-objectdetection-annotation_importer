18380c0ddc0f78bb11c4ea2f0e37f6e12cbc1d93
Peter Ing <peter.ing@outlook.com>
2021-04-17 18:24:48 +0200

    Update ei_annotation_import.py
    Fixed handling of different file types and issue with short file names.

--- ei_annotation_import.py
+++ ei_annotation_import.py
@@ -4,23 +4,22 @@
 #   training set.                                                                               #    
 #                                                                                               #        
 #   Written by Peter Ing                                                                        #
-#   April 2021                                                                                  #
+#   
+#   12 April 2021                                                                                  #
 #                                                                                               #        
 #                                                                                               #
 ################################# Enter your info below #########################################
 
 
 # Project ID is found in project Url in EI Studio
-project_id='26339' 
+project_id='insert project Id' 
 
 # API key is found under keys in studio 
-api_key = 'ei_d6a8fb1a4b9f4a2dcded6f306edd802ec4a2e571d797bf67905387c996c1a88c'
+api_key = 'insert api key'
 
 # Dataset size
 train_imageset_size = 33 # insert the number of training images displayed in EI Studio
 test_imageset_size =  9  # insert the number of test images displayed in EI Studio
-
-imagefiletype = 'jpg'   # specify image filetype extension
 
 # Relative path to this file ( ideally place this file in folder that contains image data and 
 # training data located in folders called "images" and "labels respectively")
@@ -47,7 +46,7 @@ import xml.etree.ElementTree as ET
 data_collected_size = train_imageset_size + test_imageset_size
 
 def parseVOC(filename):
-    filename = path_labels + filename[:-3]+'xml'
+    filename = path_labels + filename +'.xml'
     xmldata = ET.parse(filename)
     xmlroot = xmldata.getroot()
     label = xmlroot.find('object/name').text
@@ -71,33 +70,33 @@ response = requests.request("GET", ei_list_samples_url, headers=headers, params=
 ei_samples = json.loads(response.text)
 
 
-try:
-    for i in ei_samples["samples"]:
-        filenamelen =  i["filename"].find(imagefiletype) + len(imagefiletype)
-        filename = i["filename"][:filenamelen]
-        bndbox = parseVOC(filename)
-        ei_api = ei_set_boundingboxes_url + str(i["id"]) + "/bounding-boxes"
-        payload = {"boundingBoxes": [
-            {
-                "label": bndbox[0],
-                "x": bndbox[1],
-                "y": bndbox[2],
-                "width": bndbox[3],
-                "height": bndbox[4]
-            }
-        ]}
-        
-        headers = {
-        "Accept": "application/json", "Content-Type": "application/json", "x-api-key": api_key
+
+for i in ei_samples["samples"]:
+    filename = i["filename"][:i["filename"].find('.')]
+    print(filename)
+    bndbox = parseVOC(filename)
+    ei_api = ei_set_boundingboxes_url + str(i["id"]) + "/bounding-boxes"
+    payload = {"boundingBoxes": [
+        {
+            "label": bndbox[0],
+            "x": bndbox[1],
+            "y": bndbox[2],
+            "width": bndbox[3],
+            "height": bndbox[4]
         }
-        print("Updating bounding box for SampleID: " + str(i["id"]))
-        response = requests.request("POST", ei_api, json=payload, headers=headers)
-        if json.loads(response.text)["success"] == True:
-            print("Connection Successfull")
-        elif json.loads(response.text)["success"] == False:
-            print('ERR:' + json.loads(response.text)["error"]) 
-except: 
-    print("ERR Check API Key and Project ID's are correct")
+    ]}
+    
+    headers = {
+    "Accept": "application/json", "Content-Type": "application/json", "x-api-key": api_key
+    }
+    print("Updating bounding box for SampleID: " + str(i["id"]))
+    response = requests.request("POST", ei_api, json=payload, headers=headers)
+    if json.loads(response.text)["success"] == True:
+        print("Connection Successfull")
+    elif json.loads(response.text)["success"] == False:
+        print('ERR:' + json.loads(response.text)["error"]) 
+
+print("ERR Check API Key and Project ID's are correct")
     
 
 print("Done please return to Edge Impulse Studio")
